42f64b67621a446b361b9ae38b209a6fa9c49f42
Amrip <58447768@qq.com>
2024-04-23 16:53:16 +0800

    :wrench: changed

--- src/components/test.vue
+++ src/components/test.vue
@@ -10,7 +10,7 @@ if (n.value) {
 <template>
     <div v-if="n === 1" />
      <div v-else-if="n === 2" />
-     <div v-else-if="n === 3" />1
+     <div v-else-if="n === 3" />
       <div v-else-if="n === 2" />
     <div v-else-if="n === 5" />
 </template>
