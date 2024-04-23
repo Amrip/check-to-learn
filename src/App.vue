fe8f076944af1e4f44911e82dcdf3c3393f5b81e
Amrip <58447768@qq.com>
2024-04-23 16:48:19 +0800

    :wrench: test lint

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <div v-if="n === 1" />
-  <div v-else-if="n === 2" />
-  <div v-else-if="n === 3" />
-  <div v-else-if="n === 2" />
-  <div v-else-if="n === 5" />
+    <div v-if="n === 1" />
+    <div v-else-if="n === 2" />
+    <div v-else-if="n === 3" />
+    <div v-else-if="n === 2" />
+    <div v-else-if="n === 5" />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/test.vue
+++ src/components/test.vue
@@ -1,16 +1,18 @@
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue";
 
 const n = ref(5);
-if(n.value){}
+if (n.value) {
+
+}
 </script>
 
 <template>
-  <div v-if="n === 1" />
-  <div v-else-if="n === 2" />
-  <div v-else-if="n === 3" />
-  <div v-else-if="n === 2" />
-  <div v-else-if="n === 5" />
+    <div v-if="n === 1" />
+    <div v-else-if="n === 2" />
+    <div v-else-if="n === 3" />
+    <div v-else-if="n === 2" />
+    <div v-else-if="n === 5" />
 </template>
 
 <style scoped>
